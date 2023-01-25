1a201a51bbf382537846ee66cd1ab10f1330804e
Victor <32028809+VicZhukov@users.noreply.github.com>
2023-01-25 13:58:23 +0200

    snake project

---
+++ src/main/java/org/example/MainWindow.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        setTitle("Snake");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 320);
+        setLocation(490, 180);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
