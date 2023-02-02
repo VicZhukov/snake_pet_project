558e196e93e1bd9174c927bfd4ef17dd1ca4a39f
Victor <32028809+VicZhukov@users.noreply.github.com>
2023-02-02 08:56:35 +0200

    Create pet project 'Snake'

--- src/main/java/org/example/MainWindow.java
+++ src/main/java/org/example/MainWindow.java
@@ -7,7 +7,7 @@ public class MainWindow extends JFrame {
     public MainWindow(){
         setTitle("Snake");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(320, 320);
+        setSize(320, 340);
         setLocation(490, 180);
         add(new GameField());
         setVisible(true);
